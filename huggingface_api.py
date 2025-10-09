f81a565753007f0fa468ae86b345f5035af3ca4d
kaviyashreeravikumar633-stack <kaviyashreeravikumar633@gmail.com>
2025-10-09 01:56:36 +0000

    api changed

--- huggingface_api.py
+++ huggingface_api.py
@@ -1,11 +1,12 @@
 import requests
+import os
 
 API_URLS = {
     "summary": "https://api-inference.huggingface.co/models/facebook/bart-large-cnn",
 }
 
 headers = {
-    "Authorization": "Bearer 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"
+    "Authorization": f"Bearer {os.environ.get('HUGGINGFACE_API_KEY')}"
 }
 
 def query(payload, type):
